ca439efce252a984de4b37bece77a59b22848304
mironeck <mironec@tut.by>
2017-03-20 17:13:53 +0300

    add task 2 from sorting

--- p005/src/main/java/ru/mironenko/collectionslite/sortings/SortUser.java
+++ p005/src/main/java/ru/mironenko/collectionslite/sortings/SortUser.java
@@ -1,13 +1,19 @@
 package ru.mironenko.collectionslite.sortings;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import com.google.common.hash.HashCode;
+
+import java.util.*;
 
 /**
  * Created by nikita on 20.03.2017.
  */
+
+//В классе SortUser из 1-го задания необходимо написать два метода:
+//public List<User> sortHash (List<User>) - в этом методе необходимо определить Comparator для метода Collections.sort
+//и отсортировать List<User> по hash-коду.
+//public List<User> sortLength (List<User>) -
+// в этом методе необходимо определить Comparator для метода Collections.sort
+//и отсортировать List<User> по длине имени.
 public class SortUser {
 
     /**
@@ -25,6 +31,34 @@ public class SortUser {
         return result;
     }
 
+    public List<User> sortLength (List<User> list) {
+
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+
+                Integer one = o1.getName().length();
+                Integer two = o2.getName().length();
+
+                return one.compareTo(two);
+            }
+        });
+
+        return list;
+    }
+
+    public List<User> sortHash (List<User> list) {
+
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+
+                return o1.hashCode() - o2.hashCode();
+            }
+        });
+        return list;
+    }
+    
 }
 
 
--- p005/src/test/java/ru/mironenko/collectionslite/sortings/SortUserTest.java
+++ p005/src/test/java/ru/mironenko/collectionslite/sortings/SortUserTest.java
@@ -1,5 +1,6 @@
 package ru.mironenko.collectionslite.sortings;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -32,6 +33,41 @@ public class SortUserTest {
         assertThat(array[3].getAge(), is(4));
         assertThat(array[4].getAge(), is(5));
 
+    }
+
+    @Test
+    public void whenSortedByNameLengthUsingComparator() {
+
+        List<User> list = new ArrayList<>();
+        list.add(new User("a", 1));
+        list.add(new User("aaaa", 31));
+        list.add(new User("aa", 21));
+        list.add(new User("aaa", 10));
+
+
+        List<User> result = new SortUser().sortLength(list);
+
+        assertThat(result.get(0).getName(), is ("a"));
+        assertThat(result.get(1).getName(), is ("aa"));
+        assertThat(result.get(2).getName(), is ("aaa"));
+        assertThat(result.get(3).getName(), is ("aaaa"));
+    }
+
+    @Ignore
+    @Test
+    public void whenSortedByHashCodeUsingComparator() {
+        List<User> list = new ArrayList<>();
+        list.add(new User("a", 1));
+        list.add(new User("aaaa", 31));
+        list.add(new User("aa", 21));
+        list.add(new User("aaa", 10));
+
+        List<User> result = new SortUser().sortHash(list);
+
+        assertThat(result.get(0).getName(), is ("aaaa"));
+        assertThat(result.get(1).getName(), is ("aa"));
+        assertThat(result.get(2).getName(), is ("aaa"));
+        assertThat(result.get(3).getName(), is ("a"));
     }
 
 }
