ec7f78a619d3198f271bd7f8d5437a1f4fca9dd3
mironeck <mironec@tut.by>
2017-03-29 15:26:18 +0300

    add hasNext test

--- p005/src/test/java/ru/mironenko/collectionspro/iterator/TDAIteratorTest.java
+++ p005/src/test/java/ru/mironenko/collectionspro/iterator/TDAIteratorTest.java
@@ -21,12 +21,24 @@ public class TDAIteratorTest {
         TDAIterator tda = new TDAIterator(array);
         Iterator it = tda.iterator();
 
-        int[] result = new int[]{(int)it.next(), (int) it.next(), (int) it.next(), (int) it.next()};
-        boolean resultHasNext = it.hasNext();
+        int[] result = new int[]{(int)it.next(), (int) it.next(),(int) it.next(),(int) it.next()};
         int[] checked = new int[]{1, 2, 3, 4};
+        boolean resultHasNext = it.hasNext();
 
         assertThat(result, is (checked));
         assertThat(resultHasNext, is (false));
+    }
+
+    @Test
+    public void simple() {
+        int [][] array = new int[][]{
+                {1, 2},
+        };
+        TDAIterator tda = new TDAIterator(array);
+        Iterator it = tda.iterator();
+        it.next();
+        boolean has = it.hasNext();
+        assertThat(has, is (true));
     }
 
 }
