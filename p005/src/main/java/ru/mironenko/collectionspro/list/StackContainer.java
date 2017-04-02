1b334e86cecf39e3df8325c9c72c588827fe6734
mironeck <mironec@tut.by>
2017-04-02 16:55:00 +0300

    add stack task

---
+++ p005/src/main/java/ru/mironenko/collectionspro/list/StackContainer.java
@@ -0,0 +1,28 @@
+package ru.mironenko.collectionspro.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by nikita on 02.04.2017.
+ */
+public class StackContainer<E> extends LinkedListContainer{
+
+    /**
+     * Adds element in the head of list
+     * @param e
+     */
+    public void push(E e) {
+        addFirst(e);
+    }
+
+    /**
+     * Removes first element
+     */
+    public void pull(){
+        removeFirst();
+    }
+
+
+
+}
