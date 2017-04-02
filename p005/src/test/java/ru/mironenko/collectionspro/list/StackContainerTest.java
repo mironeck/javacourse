f17a1d2ae38e339e5750059c84c41d39421bee22
mironeck <mironec@tut.by>
2017-04-02 16:54:29 +0300

    add stack task

---
+++ p005/src/test/java/ru/mironenko/collectionspro/list/StackContainerTest.java
@@ -0,0 +1,37 @@
+package ru.mironenko.collectionspro.list;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by nikita on 02.04.2017.
+ */
+public class StackContainerTest {
+
+    @Test
+    public void whenPushElementsShouldAddInTheHead(){
+
+        StackContainer<Integer> sc = new StackContainer<>();
+        sc.push(3);
+        sc.push(2);
+        sc.push(1);
+
+        assertThat(sc.get(0), is(1));
+    }
+
+    @Test
+    public void whenPullElementsShouldRemovedFromTheHead(){
+
+        StackContainer<Integer> sc = new StackContainer<>();
+        sc.push(3);
+        sc.push(2);
+        sc.push(1);
+
+        sc.pull();
+
+        assertThat(sc.get(0), is(2));
+    }
+
+}
