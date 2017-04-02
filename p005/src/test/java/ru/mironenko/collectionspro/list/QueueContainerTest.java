3b715d22f727f1126ef9376f0c82277033ac4a39
mironeck <mironec@tut.by>
2017-04-02 17:16:01 +0300

    add qeueu task

---
+++ p005/src/test/java/ru/mironenko/collectionspro/list/QueueContainerTest.java
@@ -0,0 +1,38 @@
+package ru.mironenko.collectionspro.list;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by nikita on 02.04.2017.
+ */
+public class QueueContainerTest {
+
+    @Test
+    public void whenPushElementsShouldAddInTheTail(){
+
+        QueueContainer<Integer> qc = new QueueContainer<>();
+        qc.push(1);
+        qc.push(2);
+        qc.push(3);
+
+        assertThat(qc.get(0), is(1));
+    }
+
+    @Test
+    public void whenPullElementsShouldRemovedFromTheHead(){
+
+        QueueContainer<Integer> qc = new QueueContainer<>();
+        qc.push(1);
+        qc.push(2);
+        qc.push(3);
+
+        qc.pull();
+
+        assertThat(qc.get(0), is(2));
+        assertThat(qc.getSize(), is(2));
+    }
+
+}
