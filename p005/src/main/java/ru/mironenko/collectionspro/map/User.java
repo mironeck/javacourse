1dce5ae4fecce9a6c54f880ff91afd03ea109a94
mironeck <mironec@tut.by>
2017-04-04 14:54:47 +0300

    add class User for map

---
+++ p005/src/main/java/ru/mironenko/collectionspro/map/User.java
@@ -0,0 +1,20 @@
+package ru.mironenko.collectionspro.map;
+
+/**
+ * Created by nikita on 04.04.2017.
+ */
+//Создать модель User и три поля String name, int children, Calendar birthday,
+
+public class User {
+
+    private String name;
+    private int children;
+    private int birthday;
+
+    public User(String name, int children, int birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+}
