57350818c31be680dbac96873b779f4df84953a8
mironeck <mironec@tut.by>
2017-04-23 09:51:17 +0300

    add parse XML

--- p005/src/main/java/ru/mironenko/collectionspro/controltasks/orderbook/OrdersDOMBuilder.java
+++ p005/src/main/java/ru/mironenko/collectionspro/controltasks/orderbook/OrdersDOMBuilder.java
@@ -2,28 +2,33 @@ package ru.mironenko.collectionspro.controltasks.orderbook;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import ru.mironenko.collectionspro.tree.SimpleBinarySearchTree;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Created by nikita on 20.04.2017.
  */
 public class OrdersDOMBuilder {
 
-    private Set<Order> orders;
+    private Map<String, List<Order>> orders;
+    private List<Order> bookOneList;
+    private List<Order> bookTwoList;
+    private List<Order> bookThreeList;
+
     private DocumentBuilder docBuilder;
 
     public OrdersDOMBuilder() {
-        this.orders = new TreeSet<Order>();
+        this.orders = new TreeMap<>();
+        this.bookOneList = new ArrayList<>();
+        this.bookTwoList = new ArrayList<>();
+        this.bookThreeList = new ArrayList<>();
+
         //создание DOM-анализатора
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try{
@@ -33,7 +38,7 @@ public class OrdersDOMBuilder {
         }
     }
 
-    public Set<Order> getOrders() {
+    public Map<String, List<Order>> getOrders() {
         return orders;
     }
 
@@ -47,11 +52,22 @@ public class OrdersDOMBuilder {
             Element root = doc.getDocumentElement();
             //получение списка дочерних элементов <order>
             NodeList orderList = root.getElementsByTagName("AddOrder");
-            for(int i = 0; i < 20; i++) {
+            for(int i = 0; i < orderList.getLength(); i++) {
                 Element orderElement = (Element) orderList.item(i);
                 Order order = buildOrder(orderElement);
-                orders.add(order);
+                if(order.getBook().equals("book-1")){
+                    bookOneList.add(order);
+                } else if(order.getBook().equals("book-2")) {
+                    bookTwoList.add(order);
+                } else if(order.getBook().equals("book-3")) {
+                    bookThreeList.add(order);
+                }
             }
+
+            orders.put("book-1", bookOneList);
+            orders.put("book-2", bookTwoList);
+            orders.put("book-3", bookThreeList);
+
         } catch (IOException e) {
             System.err.println("IOException " + e);
         } catch (SAXException e) {
@@ -63,27 +79,38 @@ public class OrdersDOMBuilder {
 
     private Order buildOrder(Element orderElement) {
 
-        Integer id = Integer.parseInt(getElementTextContent(orderElement, "id"));
+        String bookName = getElementTextContent(orderElement, "book");
+        Integer id = Integer.parseInt(getElementTextContent(orderElement, "orderId"));
         String operation = getElementTextContent(orderElement, "operation");
         Double price = Double.parseDouble(getElementTextContent(orderElement, "price"));
         Integer volume = Integer.parseInt(getElementTextContent(orderElement, "volume"));
-        Order order = new Order(id, operation, price, volume);
+        Order order = new Order(bookName, id, operation, price, volume);
         return order;
     }
 
-    private static String getElementTextContent(Element element, String elementName) {
+    private static String getElementTextContent(Element element, String attributeName) {
 
-        NodeList nodeList = element.getElementsByTagName(elementName);
-        Node node = nodeList.item(0);
-        String text = node.getTextContent();
+        String text = element.getAttribute(attributeName);
         return text;
     }
 
     public static void main(String[] args) {
 
         OrdersDOMBuilder domBuilder = new OrdersDOMBuilder();
-        domBuilder.buildSetOrders("D:\\orders.xml");
-        System.out.println(domBuilder.getOrders());
+        domBuilder.buildSetOrders("D:\\ordersTest.xml");
+
+        for(Order tmp : domBuilder.getOrders().get("book-1")) {
+            System.out.println(tmp.getBook() + " " + tmp.getId() + " " + tmp.getOperation() + " " + tmp.getPrice() + " " + tmp.getVolume());
+        }
+
+        for(Order tmp : domBuilder.getOrders().get("book-2")) {
+            System.out.println(tmp.getBook() + " " + tmp.getId() + " " + tmp.getOperation() + " " + tmp.getPrice() + " " + tmp.getVolume());
+        }
+
+        for(Order tmp : domBuilder.getOrders().get("book-3")) {
+            System.out.println(tmp.getBook() + " " + tmp.getId() + " " + tmp.getOperation() + " " + tmp.getPrice() + " " + tmp.getVolume());
+        }
     }
+
 
 }
