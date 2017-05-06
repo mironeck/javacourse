b8210f4a2e678a498c1444ff18856234ac1da170
mironeck <mironec@tut.by>
2017-05-06 13:07:45 +0300

    add control class

--- p005/src/main/java/ru/mironenko/collectionspro/controltasks/orderbook/ControlClass.java
+++ p005/src/main/java/ru/mironenko/collectionspro/controltasks/orderbook/ControlClass.java
@@ -13,12 +13,25 @@ import java.util.*;
  */
 public class ControlClass {
 
+    /**
+     * Creates two sets: for BID and for ASK.
+     * @param fileName
+     */
     public void getBIDandASK(String fileName) {
 
+        /**
+         * Set for BID
+         */
         Set<Order> setForBID = new TreeSet<>();
 
+        /**
+         * Set for ASK
+         */
         Set<Order> setForASK = new TreeSet<>();
 
+        /**
+         * SAXBuilder for Orders
+         */
         OrdersSAXBuilder ordersSAXBuilder = new OrdersSAXBuilder();
         Map<String, ArrayList<Order>> mapOrders = ordersSAXBuilder.buildMapOrders(fileName);
 
@@ -28,31 +41,20 @@ public class ControlClass {
 
                 if("BUY".equals(order.getOperation())) {
 
-                    //checkForSamePrice(setForBID, order);
-                    setForBID.add(order);
-                    //mapForBID.put(order.getId(), order);
+                        setForBID.add(order);
                 }
 
                 if("SELL".equals(order.getOperation())) {
 
-//                    Order neworder = null;
-//                    for(Order check : setForASK) {
-//                        if(order.getPrice() == check.getPrice()) {
-//                            neworder = new Order(order.getBook(), order.getOperation(), order.getPrice(), (order.getVolume()+ check.getVolume()) , order.getId());
-//
-//                            setForASK.add(neworder);
-//                            setForASK.remove(check);
-//                        }
-//                    }
-
                     setForASK.add(order);
-                    //mapForASK.put(order.getId(), order);
                 }
 
             }
 
             System.out.println("BUY " +  " PRICE " + "  SELL " + "for book = " + tmp);
             setForBID = checkForSamePrice(setForBID);
+            //setForBID = sortSetFromHightoLow(setForBID);
+
             //mapForBID = checkForSamePrice(mapForBID);
 
             for(Order temp : setForBID) {
@@ -60,43 +62,38 @@ public class ControlClass {
             }
 
             setForASK = checkForSamePrice(setForASK);
+            //setForASK = sortSetFromLowHighto(setForASK);
             //mapForASK = checkForSamePrice(mapForASK);
             for(Order temp : setForASK) {
                 System.out.println("           " + temp.getPrice() + " " + temp.getVolume());
             }
 
-//            setForASK.clear();
-//            setForBID.clear();
-
             setForBID.clear();
             setForASK.clear();
-
         }
-
     }
 
     private Set<Order> checkForSamePrice(Set<Order> orders) {
 
-        ArrayList<Order> startOrder = new ArrayList<Order>(orders);
-
         int volume = 0;
-        Set<Order> result = new TreeSet<Order>(new Comparator<Order>() {
-            @Override
-            public int compare(Order o1, Order o2) {
-                if(o1.getPrice() < o2.getPrice()){
-                    return -1;
-                } else if(o1.getPrice() > o2.getPrice()) {
-                    return 1;
-                }
-                return 0;
-            }
-        });
+        Set<Order> result = new TreeSet<Order>();
+//        (new Comparator<Order>() {
+//            @Override
+//            public int compare(Order o1, Order o2) {
+//                if(o1.getPrice() < o2.getPrice()){
+//                    return -1;
+//                } else if(o1.getPrice() > o2.getPrice()) {
+//                    return 1;
+//                }
+//                return 0;
+//            }
+//        });
         Order newOrder = null;
         boolean flag = false;
 
-        for(Order temp : startOrder) {
+        for(Order temp : orders) {
             volume = temp.getVolume();
-            for (Order check : startOrder) {
+            for (Order check : orders) {
                 if ((temp.getId() != check.getId()) && (temp.getPrice() == check.getPrice())) {
                     volume += check.getVolume();
 
@@ -117,26 +114,41 @@ public class ControlClass {
 
     }
 
-//    public static Set<Order> sortSetFromHightoLow(Set<Order> orders) {
-//
-//        for(Order tmp : orders) {
-//
-//        }
-//       Set<Order> sortedSet = new TreeSet<Order>(new Comparator<Order>() {
-//           @Override
-//           public int compare(Order o1, Order o2) {
-//               if(o1.getPrice() < o2.getPrice()){
-//                   return -1;
-//               } else if(o1.getPrice() > o2.getPrice()) {
-//                   return 1;
-//               }
-//               return 0;
-//           }
-//       });
-//
-//        sortedSet.addAll(orders);
-//        return sortedSet;
-//    }
+    public static Set<Order> sortSetFromHightoLow(Set<Order> orders) {
+
+       Set<Order> sortedSet = new TreeSet<Order>(new Comparator<Order>() {
+           @Override
+           public int compare(Order o1, Order o2) {
+               if(o1.getPrice() < o2.getPrice()){
+                   return -1;
+               } else if(o1.getPrice() > o2.getPrice()) {
+                   return 1;
+               }
+               return 0;
+           }
+       });
+
+        sortedSet.addAll(orders);
+        return sortedSet;
+    }
+
+    public static Set<Order> sortSetFromLowHighto(Set<Order> orders) {
+
+        Set<Order> sortedSet = new TreeSet<Order>(new Comparator<Order>() {
+            @Override
+            public int compare(Order o1, Order o2) {
+                if(o1.getPrice() < o2.getPrice()){
+                    return 1;
+                } else if(o1.getPrice() > o2.getPrice()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+
+        sortedSet.addAll(orders);
+        return sortedSet;
+    }
 
     public static void main(String[] args) {
 
