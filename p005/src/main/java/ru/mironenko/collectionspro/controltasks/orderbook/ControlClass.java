7ff8aecce006c9f17e92fbb1467c214193fa8874
mironeck <mironec@tut.by>
2017-05-07 16:40:27 +0300

    add changes to conntrol class

--- p005/src/main/java/ru/mironenko/collectionspro/controltasks/orderbook/ControlClass.java
+++ p005/src/main/java/ru/mironenko/collectionspro/controltasks/orderbook/ControlClass.java
@@ -1,11 +1,5 @@
 package ru.mironenko.collectionspro.controltasks.orderbook;
 
-import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry;
-import com.sun.corba.se.pept.transport.InboundConnectionCache;
-import com.sun.org.apache.xpath.internal.operations.Or;
-import sun.reflect.generics.tree.DoubleSignature;
-import sun.reflect.generics.tree.Tree;
-
 import java.util.*;
 
 /**
@@ -14,7 +8,7 @@ import java.util.*;
 public class ControlClass {
 
     /**
-     * Creates two sets: for BID and for ASK.
+     * Creates two sets: for BID and for ASK and prints it.
      * @param fileName
      */
     public void getBIDandASK(String fileName) {
@@ -52,19 +46,17 @@ public class ControlClass {
             }
 
             System.out.println("BUY " +  " PRICE " + "  SELL " + "for book = " + tmp);
-            setForBID = checkForSamePrice(setForBID);
-            //setForBID = sortSetFromHightoLow(setForBID);
+            Set<Order> setBID = checkForSamePrice(setForBID);
+            Set<Order> sortedSetForBID = sortSetFromHightoLow(setBID);
 
-            //mapForBID = checkForSamePrice(mapForBID);
-
-            for(Order temp : setForBID) {
+            for(Order temp : sortedSetForBID) {
                 System.out.println(temp.getPrice() + " " + temp.getVolume());
             }
 
-            setForASK = checkForSamePrice(setForASK);
-            //setForASK = sortSetFromLowHighto(setForASK);
-            //mapForASK = checkForSamePrice(mapForASK);
-            for(Order temp : setForASK) {
+            Set<Order> setASK = checkForSamePrice(setForASK);
+            Set<Order> sortedSetForASK = sortSetFromLowHighto(setASK);
+
+            for(Order temp : sortedSetForASK) {
                 System.out.println("           " + temp.getPrice() + " " + temp.getVolume());
             }
 
@@ -73,30 +65,22 @@ public class ControlClass {
         }
     }
 
+    /**
+     * Finds orders with same price, sum volumes, creates new order with this volume and puts in new Set.
+     * @param orders
+     * @return
+     */
     private Set<Order> checkForSamePrice(Set<Order> orders) {
 
         int volume = 0;
         Set<Order> result = new TreeSet<Order>();
-//        (new Comparator<Order>() {
-//            @Override
-//            public int compare(Order o1, Order o2) {
-//                if(o1.getPrice() < o2.getPrice()){
-//                    return -1;
-//                } else if(o1.getPrice() > o2.getPrice()) {
-//                    return 1;
-//                }
-//                return 0;
-//            }
-//        });
         Order newOrder = null;
         boolean flag = false;
-
         for(Order temp : orders) {
             volume = temp.getVolume();
             for (Order check : orders) {
                 if ((temp.getId() != check.getId()) && (temp.getPrice() == check.getPrice())) {
                     volume += check.getVolume();
-
                     flag = true;
                 }
             }
@@ -109,20 +93,23 @@ public class ControlClass {
                 result.add(temp);
             }
         }
-
         return result;
-
     }
 
+    /**
+     * Sorts set of orders from higher price to lower price
+     * @param orders
+     * @return new sorted set
+     */
     public static Set<Order> sortSetFromHightoLow(Set<Order> orders) {
 
        Set<Order> sortedSet = new TreeSet<Order>(new Comparator<Order>() {
            @Override
            public int compare(Order o1, Order o2) {
                if(o1.getPrice() < o2.getPrice()){
-                   return -1;
-               } else if(o1.getPrice() > o2.getPrice()) {
                    return 1;
+               } else if(o1.getPrice() > o2.getPrice()) {
+                   return -1;
                }
                return 0;
            }
@@ -132,26 +119,30 @@ public class ControlClass {
         return sortedSet;
     }
 
+    /**
+     * Sorts set of orders from lower price to higher price
+     * @param orders
+     * @return new sorted set
+     */
     public static Set<Order> sortSetFromLowHighto(Set<Order> orders) {
 
         Set<Order> sortedSet = new TreeSet<Order>(new Comparator<Order>() {
             @Override
             public int compare(Order o1, Order o2) {
                 if(o1.getPrice() < o2.getPrice()){
-                    return 1;
-                } else if(o1.getPrice() > o2.getPrice()) {
                     return -1;
+                } else if(o1.getPrice() > o2.getPrice()) {
+                    return 1;
                 }
                 return 0;
             }
         });
-
         sortedSet.addAll(orders);
         return sortedSet;
     }
 
     public static void main(String[] args) {
 
-        new ControlClass().getBIDandASK("D:\\ordersTest.xml");
+        new ControlClass().getBIDandASK("D:\\orders.xml");
     }
 }
