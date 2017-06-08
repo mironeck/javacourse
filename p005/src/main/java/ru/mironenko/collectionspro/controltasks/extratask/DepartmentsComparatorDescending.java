b3861147bdf9d8a6cd40ac7403703a68fd186a5e
mironeck <mironec@tut.by>
2017-06-08 21:20:48 +0300

    add comp desc

--- p005/src/main/java/ru/mironenko/collectionspro/controltasks/extratask/DepartmentsComparatorDescending.java
+++ p005/src/main/java/ru/mironenko/collectionspro/controltasks/extratask/DepartmentsComparatorDescending.java
@@ -13,15 +13,15 @@ public class DepartmentsComparatorDescending implements Comparator<Department> {
         String s1 = o1.getName().substring(0, 2);
         String s2 = o2.getName().substring(0, 2);
 
-        String s3 = null;
-        String s4 = null;
-
-        if (o1.getName().length() == 6) {
-            s3 = o1.getName().substring(3);
-        }
-        if (o2.getName().length() == 6) {
-            s4 = o2.getName().substring(3);
-        }
+//        String s3 = null;
+//        String s4 = null;
+//
+//        if (o1.getName().length() == 6) {
+//            s3 = o1.getName().substring(3);
+//        }
+//        if (o2.getName().length() == 6) {
+//            s4 = o2.getName().substring(3);
+//        }
 
 
         if( !(s1.equals(s2)) ) {
