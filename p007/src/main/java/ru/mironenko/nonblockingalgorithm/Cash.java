d5a04b144bed8df6bfd0c84dd4513d90e7203d19
mironeck <mironec@tut.by>
2017-06-14 15:09:54 +0300

    add cash task

--- p007/src/main/java/ru/mironenko/nonblockingalgorithm/Cash.java
+++ p007/src/main/java/ru/mironenko/nonblockingalgorithm/Cash.java
@@ -1,7 +1,74 @@
 package ru.mironenko.nonblockingalgorithm;
 
+import ru.mironenko.monitore.taskfour.Main;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+
 /**
  * Created by nikita on 12.06.2017.
  */
+//1. Необходимо сделать кеш для хранение моделей. в кеше должны быть методы add, update, delete.
+//2. Кеш должен работать на неблокирующих алгоритмах. - использовать ConcurrentHashMap
+//3. В кеше должна быть возможность проверять валидность данных.
+// Например. Два пользователя прочитали данные task_1
+// первый пользователь изменил поле имя и второй сделал тоже самое.
+// нужно перед обновлением данных проверить. что текущий пользователь не затер данные другого пользователя.
+// если данные затерты, то выбросить OplimisticException - такая реализация достигается за счет введения
+// в модель поля version и перед обновлением данных проверять текущую версию и ту,
+// что обновляем и увеличивать на один каждый раз когда обновили. если версии не равны то кидать исключение.
+// Использовать метод
+// https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ConcurrentHashMap.html#computeIfPresent-K-java.util.function.BiFunction-
+
+
 public class Cash {
+
+    private final ConcurrentHashMap<Integer, Model> map = new ConcurrentHashMap<>();
+
+    /**
+     * Adds model to map.
+     * @param model
+     */
+    public void add(Model model){
+
+        this.map.put(model.getId(), model);
+    }
+
+    /**
+     * The method sets new name to model
+     * @param newModel model with new name
+     */
+    public void update(Model newModel){
+        this.map.computeIfPresent(newModel.getId(), new BiFunction<Integer, Model, Model>(){
+
+            @Override
+            public Model apply(Integer integer, Model oldModel) {
+                if(oldModel.getVersion() == newModel.getVersion()) {
+                    oldModel.setName(newModel.getName());
+                } else {
+                    throw new OptimisticException("wrong version");
+                }
+                return oldModel;
+            }
+        });
+    }
+
+    /**
+     * The method removes model from map
+     * @param modelToDelete model to remove
+     */
+    public void delete(Model modelToDelete){
+        this.map.remove(modelToDelete.getId());
+    }
+
+    /**
+     * Exception for update method
+     */
+
+    private class OptimisticException extends RuntimeException {
+
+        public OptimisticException(String s) {
+            super(s);
+        }
+    }
 }
--- p007/src/main/java/ru/mironenko/nonblockingalgorithm/Model.java
+++ p007/src/main/java/ru/mironenko/nonblockingalgorithm/Model.java
@@ -3,38 +3,84 @@ package ru.mironenko.nonblockingalgorithm;
 /**
  * Created by nikita on 12.06.2017.
  */
-public class Task {
+public class Model {
 
+    /**
+     * id of model
+     */
     private int id;
+    /**
+     * Name of model
+     */
     private String name;
-    private int version = 0;
+    /**
+     * Version of model
+     */
+    private volatile int version = 0;
 
-    public Task(int id, String name) {
+    public Model(){}
+
+    /**
+     * Constructor of Model class
+     * @param id - id of model
+     * @param name - name of model
+     */
+    public Model(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Getter of id
+     * @return id of model
+     */
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     * Getter of name
+     * @return name of model
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter of name. Sets new name to model and inc version of model
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
+        this.version++;
     }
+
+    /**
+     * Getter of models version
+     */
 
     public int getVersion() {
-        return version;
+        return this.version;
     }
 
-    public void setVersion(int version) {
-        this.version = version;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Model)) return false;
+
+        Model model = (Model) o;
+
+        if (getId() != model.getId()) return false;
+        if (getVersion() != model.getVersion()) return false;
+        return getName() != null ? getName().equals(model.getName()) : model.getName() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + getVersion();
+        return result;
     }
 }
