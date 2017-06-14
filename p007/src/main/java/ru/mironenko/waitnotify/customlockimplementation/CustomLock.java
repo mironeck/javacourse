77162246c790b160c0c435559ece480386ff0f86
mironeck <mironec@tut.by>
2017-06-14 09:56:03 +0300

    add custom lock implementation

--- p007/src/main/java/ru/mironenko/waitnotify/customlockimplementation/CustomLock.java
+++ p007/src/main/java/ru/mironenko/waitnotify/customlockimplementation/CustomLock.java
@@ -3,5 +3,28 @@ package ru.mironenko.waitnotify.customlockimplementation;
 /**
  * Created by nikita on 12.06.2017.
  */
+//Реализовать собственный механизм блокировок Lock. на основе synch wait notify
+
+/**
+ * Custom lock implementation
+ */
 public class CustomLock {
+
+    /**
+     * Flag
+     */
+    private boolean isLocked = false;
+
+    public synchronized void lock() throws InterruptedException{
+
+        while(isLocked) {
+            wait();
+        }
+        isLocked = true;
+    }
+
+    public synchronized void unLock(){
+        isLocked = false;
+        notify();
+    }
 }
