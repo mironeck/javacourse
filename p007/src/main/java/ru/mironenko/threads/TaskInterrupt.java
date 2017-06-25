0c4fa5404e7e921e83fb7b90f6beb6d1989a4cde
mironeck <mironec@tut.by>
2017-06-25 12:00:58 +0300

    add interrupttask

--- p007/src/main/java/ru/mironenko/threads/TaskInterrupt.java
+++ p007/src/main/java/ru/mironenko/threads/TaskInterrupt.java
@@ -8,19 +8,25 @@ public class TaskInterrupt extends Thread {
     public void run(){
 
         try {
-            Thread.sleep(1000);
-            System.out.println("Task");
+            for(int i = 0; i < 10; i++) {
+                if(Thread.currentThread().isInterrupted()) {
+                    System.out.printf("Thread %s is interrupted \n", Thread.currentThread().getName() );
+                } else {
+                    Thread.sleep(1000);
+                    System.out.println("Task");
+                }
+            }
         } catch (InterruptedException e) {
             System.out.println("Exception handled " + e);
         }
         System.out.println("Thread running");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         TaskInterrupt taskInterrupt = new TaskInterrupt();
         taskInterrupt.start();
-
+        Thread.sleep(5000);
         taskInterrupt.interrupt();
 
     }
