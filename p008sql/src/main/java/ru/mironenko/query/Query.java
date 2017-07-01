ecf07dd755d417116d20366cb3463b5c8d60e8aa
mironeck <mironec@tut.by>
2017-07-01 07:09:45 +0300

    UML системы [ #1093]

---
+++ p008sql/src/main/java/ru/mironenko/query/Query.java
@@ -0,0 +1,17 @@
+package main.java.ru.mironenko.query;
+
+/**
+ * Created by nikita on 29.06.2017.
+ */
+
+public class Query {
+
+
+    public String quetyToDB() {
+        String result = "";
+
+        
+
+        return result;
+    }
+}
