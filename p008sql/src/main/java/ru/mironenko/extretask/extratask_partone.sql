ae5672d302479d2d2e7fa9a702f91239b6008905
mironeck <mironec@tut.by>
2017-07-14 20:12:39 +0300

    Дополнительное задание [ #2760]. Запросы

---
+++ p008sql/src/main/java/ru/mironenko/extretask/extratask_partone.sql
@@ -0,0 +1,3 @@
+﻿select p.name, c.name, c.id 
+from person as p inner join company as c 
+on p.company_id = c.id and p.company_id != 5;
---
+++ p008sql/src/main/java/ru/mironenko/extretask/extratask_parttwo.sql
@@ -0,0 +1,5 @@
+﻿select company.name, count(*) as cnt
+from company, person where person.company_id = company.id
+group by company.name
+order by cnt desc
+limit 1;
